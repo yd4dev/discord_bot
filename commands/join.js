e12b2d4b6c76cac92d9d3bdf52f99efce07bd85e
Dennis1507 <yildirimdennis@aol.de>
2020-12-10 18:31:18 +0100

    A join.js Command for Bot Owner Testing

---
+++ commands/join.js
@@ -0,0 +1,14 @@
+module.exports = {
+	name: 'join',
+    description: 'A simple test command.',
+    args: false,
+	execute(message, args, client, prefix) {
+
+        if(message.author.id != '104537226192371712') return message.channel.send('Sorry, this command is restricted to the bot\'s owner.')
+        if(!args[0]) return message.channel.send('Please provide a voice channel id.')
+        if(message.guild.channels.cache.find(channel => channel.id == args[0]).type != 'voice') return message.channel.send('This is not a voice channel.')
+        
+        message.guild.channels.cache.find(channel => channel.id == args[0]).join()
+
+	},
+};
