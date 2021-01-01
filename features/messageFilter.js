6a7d749893fc418e981fdd58a8160ed14e59929b
Dennis1507 <yildirimdennis@aol.de>
2021-01-01 02:49:05 +0100

    Message Filter on Edited Message

--- features/messageFilter.js
+++ features/messageFilter.js
@@ -18,5 +18,25 @@
             message.delete();
 
         }
-    })
+      })
+      
+      client.on('messageUpdate', async (oldMessage, message) => {
+
+        try {
+            if (message.member.hasPermission('MANAGE_MESSAGES')) return
+        } catch (err) {
+            console.log('ERROR + \n' + err)
+            console.log('MESSAGE THAT CAUSED THE ERROR: \n' + message)
+            console.log('ERROR END \n')
+        }
+  
+      let result = await client.schemas.get('guild').findOne({ _id: message.member.guild.id})  
+          
+      if (!result.bannedWords) return;        
+  
+      if (result.bannedWords.some(word => message.content.toLowerCase().replace(/\s+/g, '').includes(word.replace(/\s+/g, '')))) {
+          message.delete();
+
+      }
+  })
 }
