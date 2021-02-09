537a0b8a4a6031a401ce9e39544dcffdae3e3f66
Dennis1507 <yildirimdennis@aol.de>
2021-02-09 22:02:05 +0100

    Fixed Mute Command

--- commands/moderation/mute.js
+++ commands/moderation/mute.js
@@ -1,91 +1,96 @@
 module.exports = {
 	name: 'mute',
-	description: 'Lets you mute users.',
-	args: 2,
+	description: 'Restrict users from writing into text channels.',
+	args: 1,
 	guild: true,
-	permissions: 'MANAGE_MESSAGES',
+	permissions: 'MANAGE_ROLES',
 	usage: '%prefixmute [@Target] [Time]',
 	async execute(message, args, client, prefix) {
 
-		args.shift();
-		const target = message.mentions.members.first();
+		const target = message.mentions.members.first()
 
-		if(!target) return message.reply('please provide a user.')
+		if (args[0].search(target.id)) args.shift()
+		else client.commands.help.commandHelp(message, 'mute', prefix, client)
+
+		if (!target) return message.reply('please mention a member.')
 
 		const currentlyMuted = await client.schemas.get('mute').find({
 			guildId: message.guild.id,
 			userId: target.id,
 		})
 
-		if(currentlyMuted.length) return message.reply('that user is already muted.');
+		if (currentlyMuted.length) return message.reply('that user is already muted.')
 
-		if(target.roles.highest.comparePositionTo(message.guild.member(client.user).roles.highest) >= 0) return message.channel.send('It seems that I am not high enough in the role hierarchy to mute that member.')
-		if(target.roles.highest.comparePositionTo(message.member.roles.highest) >= 0) return message.channel.send('You cannot mute members that are higher than you.')
+		if (target.roles.highest.comparePositionTo(message.guild.member(client.user).roles.highest) >= 0) return message.channel.send('It seems that my highest role is not high enough to mute that member.')
+		if (target.roles.highest.comparePositionTo(message.member.roles.highest) >= 0) return message.channel.send('You cannot mute members that are higher than you.')
 
-		const time = args[0].split(/(?<=[A-Za-z])/);
+		if (args[0]) {
 
-		const types = {
-			m: 60,
-			h: 3600,
-			d: 86400,
-		}
+			const time = args[0].split(/(?<=[A-Za-z])/)
 
-		for (let i = 0; i < time.length; i++) {
-
-			const element = time[i];
-
-			const elementType = element.slice(-1);
-
-			if (!types.hasOwnProperty(elementType)) {
-				message.reply('Please provide a valid duration.');
-				break;
+			const types = {
+				m: 60,
+				h: 3600,
+				d: 86400,
 			}
 
+			let timestamp = Date.now()
+
+			time.forEach(element => {
+				const elementType = element.slice(-1)
+
+				if (!types.hasOwnProperty(elementType)) {
+					message.reply('Please provide a valid duration.')
+					return timestamp = false
+				}
+
+				const number = element.substring(0, element.length - 1)
+
+				timestamp = timestamp + number * types[elementType] * 1000
+			})
+
+			if (!timestamp) return console.log('Please provide a valid duration.')
+
+			const expirationDate = new Date(timestamp)
+
+			const mutedRole = message.guild.roles.cache.find(role => role.name === 'Muted')
+
+			const everyoneRole = message.guild.roles.cache.find(role => role.name === '@everyone')
+
+			const userRoles = target.roles.cache.array()
+
+			userRoles.splice(userRoles.indexOf(everyoneRole, 1))
+
+			if(!mutedRole) return message.channel.send(`I could not find a \`Muted\` role. Let me set up one by using \`${prefix}setup mute\``)
+
+			await client.schemas.get('mute').findOneAndUpdate({
+
+				guildId: message.guild.id,
+				userId: target.id,
+			}, {
+
+				guildId: message.guild.id,
+				userId: target.id,
+				moderatorId: message.author.id,
+				userRoles: userRoles,
+				expires: expirationDate,
+			}, {
+				upsert: true,
+			})
+
+			userRoles.forEach(element => {
+				target.roles.remove(element)
+			})
+
+			target.roles.add(mutedRole)
+
+			message.channel.send(`I have muted ${target} until ${expirationDate.toDateString()}, ${expirationDate.toTimeString()}`)
+
+		}
+		else {
+			return message.channel.send('Please provide a duration.')
 		}
 
-		let timestamp = Date.now();
-
-		time.forEach(function(element) {
-			const elementType = element.slice(-1);
-			const number = element.substring(0, element.length - 1);
-
-			timestamp = timestamp + number * types[elementType] * 1000;
-		})
-
-		const expirationDate = new Date(timestamp);
-
-		const mutedRole = message.guild.roles.cache.find(role => role.name === 'Muted')
-
-		const everyoneRole = message.guild.roles.cache.find(role => role.name === '@everyone')
-
-		const userRoles = target.roles.cache.array();
-
-		userRoles.splice(userRoles.indexOf(everyoneRole, 1))
-
-		if(!mutedRole) return message.channel.send(`I could not find a \`Muted\` role. Let me set up one by using \`${prefix}setup mute\``)
-
-		await client.schemas.get('mute').findOneAndUpdate({
-
-			guildId: message.guild.id,
-			userId: target.id,
-		}, {
-
-			guildId: message.guild.id,
-			userId: target.id,
-			moderatorId: message.author.id,
-			userRoles: userRoles,
-			expires: expirationDate,
-		}, {
-			upsert: true,
-		})
-
-		userRoles.forEach(element => {
-			target.roles.remove(element)
-		});
-
-		target.roles.add(mutedRole)
-
-		message.channel.send(`I have muted ${target} until ${expirationDate.toDateString()}, ${expirationDate.toTimeString()}`);
 
 	},
-};
+}
