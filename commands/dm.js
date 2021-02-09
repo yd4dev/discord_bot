c8cc59d238e10f80b10f3f0a194871e9a9e7b912
Dennis1507 <yildirimdennis@aol.de>
2021-02-09 22:00:22 +0100

    !dm empty message #20

--- commands/dm.js
+++ commands/dm.js
@@ -8,7 +8,9 @@ module.exports = {
 
 		let user
 
-		if (client.users.cache.get(args[0])) {user = client.users.cache.get(args[0])}
+		if (client.users.cache.get(args[0])) {
+			user = client.users.cache.get(args[0])
+		}
 
 		else if (args[0].startsWith('<@') && args[0].endsWith('>')) {
 
@@ -22,13 +24,32 @@ module.exports = {
 		args.shift()
 
 		if (user) {
-			user.send(args.join(' '))
-			message.channel.send(`Message delivered to ${user.username}.`)
+			if (message.attachments.size) {
+				try {
+					user.send(args.join(' '), {
+						files: [message.attachments.first()],
+					})
+					message.channel.send(`Message delivered to ${user.username}.`)
+				}
+				catch {
+					message.channel.send('Could not deliver message.')
+				}
+			}
+			else {
+				try {
+					user.send(args.join(' '))
+					message.channel.send(`Message delivered to ${user.username}.`)
+				}
+				catch {
+					message.channel.send('Could not deliver message.')
+				}
+
+			}
+
 		}
 		else {
 			message.channel.send('I couldn\'t find that user.')
 		}
-
 
 	},
 };
