3c891330d24d416171b101510542e298941f8159
Dennis1507 <yildirimdennis@aol.de>
2021-02-16 17:52:07 +0100

    Improved Urban Search Queries

--- commands/urban.js
+++ commands/urban.js
@@ -6,46 +6,53 @@ module.exports = {
 	description: 'Search for word definitions on urban dictionary.',
 	args: true,
 	usage: '%prefixurban [word]',
-	execute(message, args) {
+	async execute(message, args) {
 
-		ud.define(args.join(' '), (error, results) => {
+		const terms = await ud.autocompleteExtra(args.join(' ')).catch(error => {
 
 			if (error?.message === 'No results founds.') return message.channel.send(`Sorry, I couldn't find: ${args.join(' ')}.`)
 
 			else if (error) return console.log(error)
 
-			let result = results[0]
-
-			if (results) {
-
-				results.forEach(r => {
-					if (r.thumbs_up > result.thumbs_up) {
-						console.log(r)
-						result = r
-					}
-				})
-
-				const Embed = new Discord.MessageEmbed()
-					.setTitle(result.word)
-					.setURL(result.permalink)
-					.setAuthor(result.author)
-					.setDescription(result.definition)
-					.addField('Example', result.example)
-					.addField('👍', result.thumbs_up, true)
-					.addField('👎', result.thumbs_down, true)
-					.setFooter(result.written_on)
-
-				try {
-					message.channel.send(Embed)
-				}
-				catch (err) {
-					console.log(err)
-				}
-
-			}
-
 		})
 
+		if (terms[0]) {
 
+			ud.define(terms[0].term, (error, results) => {
+
+				if (error?.message === 'No results founds.') return message.channel.send(`Sorry, I couldn't find: ${args.join(' ')}.`)
+
+				else if (error) return console.log(error)
+
+				let result = results[0]
+
+				if (results) {
+
+					results.forEach(r => {
+						if (r.thumbs_up > result.thumbs_up) {
+							console.log(r)
+							result = r
+						}
+					})
+
+					const Embed = new Discord.MessageEmbed()
+						.setTitle(result.word)
+						.setURL(result.permalink)
+						.setAuthor(result.author)
+						.setDescription(result.definition)
+						.addField('Example', result.example)
+						.addField('👍', result.thumbs_up, true)
+						.addField('👎', result.thumbs_down, true)
+						.setFooter(result.written_on)
+
+					try {
+						message.channel.send(Embed)
+					}
+					catch (err) {
+						console.log(err)
+					}
+				}
+			})
+		}
 	},
 };
