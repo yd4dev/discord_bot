542fb11a9aa0387000988a9215c215ec9d9d21a3
Dennis1507 <yildirimdennis@aol.de>
2021-03-01 16:59:27 +0100

    Added Link Previews to dmForwarding

--- features/dmForwarding.js
+++ features/dmForwarding.js
@@ -6,17 +6,21 @@ module.exports = client => {
 
 	client.on('message', async message => {
 
-		if (message.channel.type != 'dm') return;
-		if (message.author.bot || message.author.id === process.env.botOwnerId) return;
+		if (message.channel.type == 'dm') {
+			if (message.author.bot || message.author.id === process.env.botOwnerId) return;
 
-		const MessageDMEmbed = new Discord.MessageEmbed()
-			.setAuthor(message.author.tag, message.author.displayAvatarURL())
-			.setTimestamp(message.createdTimestamp);
+			const MessageDMEmbed = new Discord.MessageEmbed()
+				.setAuthor(message.author.tag, message.author.displayAvatarURL())
+				.setTimestamp(message.createdTimestamp);
 
-		if (message.content) MessageDMEmbed.setDescription(message.content);
-		if (message.attachments.size != 0) MessageDMEmbed.addField('Attachment:', message.attachments.first().url);
+			if (message.content) MessageDMEmbed.setDescription(message.content);
+			if (message.attachments.size != 0) MessageDMEmbed.addField('Attachment:', message.attachments.first().url);
 
-		client.users.cache.get(process.env.botOwnerId).send(MessageDMEmbed);
+			// eslint-disable-next-line no-useless-escape
+			const links = message.content.match(/[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi);
 
+			client.users.cache.get(process.env.botOwnerId).send(MessageDMEmbed);
+			if (links.length) client.users.cache.get(process.env.botOwnerId).send('Link Previews: \nhttps://' + links.join('\n https://'));
+		}
 	});
 };
