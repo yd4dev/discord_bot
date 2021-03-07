9fcd462f1c362c50ee6c58be5491f86a1a32e3fb
Dennis1507 <yildirimdennis@aol.de>
2021-03-07 13:25:25 +0100

    Enhanced lock & unlock

--- commands/moderation/lock.js
+++ commands/moderation/lock.js
@@ -3,18 +3,23 @@ module.exports = {
 	description: 'Locks a channel.',
 	args: false,
 	guild: true,
-	permissions: 'MANAGE_MESSAGES',
-	execute(message) {
+	permissions: 'MANAGE_CHANNELS',
+	execute(message, args, client) {
 
-		if (message.channel.permissionsFor(message.guild.roles.everyone).has('SEND_MESSAGES') && message.channel.permissionsFor(message.guild.roles.everyone).has('ADD_REACTIONS')) {
+		if (message.channel.permissionsFor(message.guild.roles.everyone).has('SEND_MESSAGES') || message.channel.permissionsFor(message.guild.roles.everyone).has('ADD_REACTIONS')) {
 
-			message.channel.updateOverwrite(message.guild.roles.everyone, { SEND_MESSAGES: false, ADD_REACTIONS: false });
+			if (message.guild.members.cache.get(client.user.id).permissionsIn(message.channel).has('MANAGE_CHANNELS')) {
 
-			message.channel.send('🔒 Locked the channel.');
+				message.channel.updateOverwrite(message.guild.roles.everyone, { SEND_MESSAGES: false, ADD_REACTIONS: false });
+
+				message.channel.send('🔒 Locked the channel.');
+			}
+			else {
+				message.channel.send('☠️ Could not lock the channel (Missing Permissions).');
+			}
 		}
 		else {
-			message.channel.send('🔒 This channel is already locked.');
+			message.channel.send('🔒 The channel is already locked.');
 		}
-
 	},
 };
--- commands/moderation/unlock.js
+++ commands/moderation/unlock.js
@@ -3,21 +3,23 @@ module.exports = {
 	description: 'Unlocks a channel.',
 	args: false,
 	guild: true,
-	permissions: 'MANAGE_MESSAGES',
-	execute(message) {
+	permissions: 'MANAGE_CHANNELS',
+	execute(message, args, client) {
 
-		if (message.channel.permissionsFor(message.guild.roles.everyone).has('SEND_MESSAGES') && message.channel.permissionsFor(message.guild.roles.everyone).has('ADD_REACTIONS')) {
+		if (!message.channel.permissionsFor(message.guild.roles.everyone).has('SEND_MESSAGES') || !message.channel.permissionsFor(message.guild.roles.everyone).has('ADD_REACTIONS')) {
 
-			message.channel.send('🔓 This channel is not locked.');
+			if (message.guild.members.cache.get(client.user.id).permissionsIn(message.channel).has('MANAGE_CHANNELS')) {
 
+				message.channel.updateOverwrite(message.guild.roles.everyone, { SEND_MESSAGES: null, ADD_REACTIONS: null });
+
+				message.channel.send('🔓 Unlocked the channel.');
+			}
+			else {
+				message.channel.send('☠️ Could not unlock the channel (Missing Permissions).');
+			}
 		}
 		else {
-
-			message.channel.updateOverwrite(message.guild.roles.everyone, { SEND_MESSAGES: null, ADD_REACTIONS: null });
-
-			message.channel.send('🔓 Unlocked the channel.');
-
+			message.channel.send('🔓 The channel is not locked.');
 		}
-
 	},
 };
