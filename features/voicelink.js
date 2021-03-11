a49fae9342724b752396e1caf3f77a6efae2df1c
Dennis1507 <yildirimdennis@aol.de>
2021-03-11 14:58:35 +0100

    Added console logs for debugging

--- features/voicelink.js
+++ features/voicelink.js
@@ -6,9 +6,12 @@ module.exports = client => {
 
 		const { voicelinks } = await client.schemas.get('guild').findOne({ _id: newState.guild.id });
 
+		console.log(voicelinks);
+
 		if (!voicelinks) return;
 
 		for (const k of voicelinks) {
+			console.log(k);
 			if (oldState.channel && oldState.channel.id == k[0]) {
 				if (oldState.guild.roles.cache.find(r => r.id == voicelinks.get(k[0]))) {
 					oldState.member.roles.remove(voicelinks.get(k[0]));
