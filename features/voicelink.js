44f7bc5c74a7db00b2aaa1440f14a775438f7216
Dennis1507 <yildirimdennis@aol.de>
2021-03-11 15:12:49 +0100

    Quick fix for voicelinks not working

--- features/voicelink.js
+++ features/voicelink.js
@@ -6,26 +6,22 @@ module.exports = client => {
 
 		const { voicelinks } = await client.schemas.get('guild').findOne({ _id: newState.guild.id });
 
-		console.log(voicelinks);
-
 		if (!voicelinks) return;
 
 		for (const k of voicelinks) {
-			console.log(k);
-			if (oldState.channel && oldState.channel.id == k[0]) {
-				if (oldState.guild.roles.cache.find(r => r.id == voicelinks.get(k[0]))) {
-					oldState.member.roles.remove(voicelinks.get(k[0]));
-				}
-
-			}
-			if (newState.channel && newState.channel.id == k[0]) {
-				if (newState.guild.roles.cache.find(r => r.id == voicelinks.get(k[0]))) {
-					newState.member.roles.add(voicelinks.get(k[0]));
+			if (oldState?.channel?.id === k[0]) {
+				if (oldState.guild.roles.cache.find(r => r.id == k[1])) {
+					oldState.member.roles.remove(k[1]);
 				}
 			}
-
 		}
 
+		for (const k of voicelinks) {
+			if (newState?.channel?.id === k[0]) {
+				if (newState.guild.roles.cache.find(r => r.id == k[1])) {
+					newState.member.roles.add(k[1]);
+				}
+			}
+		}
 	});
-
 };
