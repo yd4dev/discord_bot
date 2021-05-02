25d683c947123024ce809609054ff2eeeef63566
Dennis1507 <yildirimdennis@aol.de>
2021-05-02 14:08:49 +0200

    Refactor Index.js

---
+++ commands.js
@@ -0,0 +1,69 @@
+module.exports = async (message, client) => {
+	if (message.author.bot) return;
+
+	const guildresult = await client.schemas.get('guild').findOne({ _id: message.guild?.id });
+
+	const dbPrefix = guildresult?.prefix;
+
+	const prefix = dbPrefix ? dbPrefix : '!';
+
+	let slice = undefined;
+	if (message.content.startsWith(prefix)) {
+		slice = prefix.length;
+	}
+	else if (message.content.startsWith(`<@${client.user.id}>`)) {
+		slice = `<@${client.user.id}>`.length;
+	}
+	else if (message.content.startsWith(`<@!${client.user.id}>`)) {
+		slice = `<@!${client.user.id}>`.length;
+	}
+
+	if (slice) {
+
+		const args = message.content.slice(slice).trim().split(/ +/);
+
+		const commandName = args.shift().toLowerCase();
+
+		const command = client.commands.get(commandName);
+
+
+		if (!client.commands.has(commandName)) {
+			return;
+		}
+
+		if (!message.channel.permissionsFor(message.guild.me).has('SEND_MESSAGES')) {
+			return message.author.send(`I do not have enough permissions to write into ${message.channel}.`)
+				.catch(() => {
+					return;
+				});
+		}
+
+		if (guildresult?.ignoredChannels?.indexOf(message.channel.id) > -1) {
+
+			return await message.channel.send(`${message.channel} is ignored.`)
+				.then(msg => setTimeout(() => msg.delete(), 3000));
+		}
+
+		if (command.guild && message.channel.type != 'text') {
+			return message.channel.send('You can only use this command in a guild.');
+		}
+
+		if (command.permissions) {
+
+			if (command.permissions != 'BOT_OWNER' && !message.member.permissions.has(command.permissions) && message.member.id != process.env.botOwnerId) return message.channel.send('You do not have the required permissions to execute that command, <@' + message.author + '>');
+			else if (command.permissions == 'BOT_OWNER' && message.author.id != process.env.botOwnerId) return message.channel.send('This command can only be used by the bot\'s owner.');
+		}
+
+		if (args.length < command.args) {
+			return client.commands.get('help').commandHelp(message, commandName, prefix, client);
+		}
+
+		try {
+			command.execute(message, args, client, prefix);
+		}
+		catch (err) {
+			console.error(err);
+			message.channel.send(`<@${message.author.id}>there was an error while executing that command.`);
+		}
+	}
+};
--- index.js
+++ index.js
@@ -1,74 +1,16 @@
-const fs = module.require('fs');
 const Discord = require('discord.js');
-const mongo = require('./mongo.js');
-const path = require('path');
 
 require('dotenv').config();
 
-const loadFeatures = require('./features/load-features');
-
 const client = new Discord.Client({ partials: ['MESSAGE', 'CHANNEL', 'REACTION'] });
+
 client.commands = new Discord.Collection();
-
-function loadCommands(dir) {
-	fs.readdir(dir, function(err, files) {
-		if (err) throw err;
-		files.forEach(file => {
-			const filepath = path.join(dir, file);
-			fs.stat(filepath, function(err, stats) {
-				if (stats.isDirectory()) {
-					loadCommands(filepath);
-				}
-				else if (file.endsWith('.js')) {
-					console.log(`Enabling command "${file}"`);
-					const command = require('./'.concat(filepath));
-					const category = filepath.split(/\/|\\/);
-					command.category = category[category.length - 2];
-					client.commands.set(command.name, command);
-				}
-			});
-		});
-	});
-}
-
-loadCommands('./commands');
-
 client.schemas = new Discord.Collection();
 
-const schemas = fs.readdirSync('./schemas').filter(file=> file.endsWith('.js'));
-
-for (const file of schemas) {
-	const schemaName = file.substring(0, file.length - 3);
-	console.log(`Enabling schema "${schemaName}"`);
-	const schema = require(`./schemas/${file}`);
-	client.schemas.set(schemaName, schema);
-}
-
 client.once('ready', async () => {
-	loadFeatures(client);
+	require('./load.js')(client);
 
-	(() => {
-		const activities = [
-			{ name: `${client.guilds.cache.size} servers`, type: 'COMPETING' },
-			{ name: 'with Dennis#4064', type: 'PLAYING' },
-			{ name: 'you ¯\\_(ツ)_/¯', type: 'WATCHING' },
-			{ name: 'your conversations', type: 'LISTENING' },
-			{ name: 'with your data', type: 'STREAMING' },
-		];
-
-		(function setPresence() {
-
-			let activity;
-
-			do {
-				activity = activities[Math.floor(Math.random() * activities.length)];
-			} while (client.user.presence.activities[0]?.name === activity.name);
-			client.user.setActivity(activity);
-			setTimeout(setPresence, 120000);
-		})();
-	})();
-
-	await mongo();
+	await require('./mongo.js')();
 
 	client.guilds.cache.forEach(async guild => {
 		console.log(guild.name);
@@ -89,75 +31,7 @@ client.once('ready', async () => {
 });
 
 client.on('message', async message => {
-
-	if (message.author.bot) return;
-
-	const guildresult = await client.schemas.get('guild').findOne({ _id: message.guild?.id });
-
-	const dbPrefix = guildresult?.prefix;
-
-	const prefix = dbPrefix ? dbPrefix : '!';
-
-	let slice = undefined;
-	if (message.content.startsWith(prefix)) {
-		slice = prefix.length;
-	}
-	else if (message.content.startsWith(`<@${client.user.id}>`)) {
-		slice = `<@${client.user.id}>`.length;
-	}
-	else if (message.content.startsWith(`<@!${client.user.id}>`)) {
-		slice = `<@!${client.user.id}>`.length;
-	}
-
-	if (slice) {
-
-		const args = message.content.slice(slice).trim().split(/ +/);
-
-		const commandName = args.shift().toLowerCase();
-
-		const command = client.commands.get(commandName);
-
-
-		if (!client.commands.has(commandName)) {
-			return;
-		}
-
-		if (!message.channel.permissionsFor(message.guild.me).has('SEND_MESSAGES')) {
-			return message.author.send(`I do not have enough permissions to write into ${message.channel}.`)
-				.catch(() => {
-					return;
-				});
-		}
-
-		if (guildresult?.ignoredChannels?.indexOf(message.channel.id) > -1) {
-
-			return await message.channel.send(`${message.channel} is ignored.`)
-				.then(msg => setTimeout(() => msg.delete(), 3000));
-		}
-
-		if (command.guild && message.channel.type != 'text') {
-			return message.channel.send('You can only use this command in a guild.');
-		}
-
-		if (command.permissions) {
-
-			if (command.permissions != 'BOT_OWNER' && !message.member.permissions.has(command.permissions) && message.member.id != process.env.botOwnerId) return message.channel.send('You do not have the required permissions to execute that command, <@' + message.author + '>');
-			else if (command.permissions == 'BOT_OWNER' && message.author.id != process.env.botOwnerId) return message.channel.send('This command can only be used by the bot\'s owner.');
-		}
-
-		if (args.length < command.args) {
-			return client.commands.get('help').commandHelp(message, commandName, prefix, client);
-		}
-
-		try {
-			command.execute(message, args, client, prefix);
-		}
-		catch (err) {
-			console.error(err);
-			message.channel.send(`<@${message.author.id}>there was an error while executing that command.`);
-		}
-	}
-
+	require('./commands.js')(message, client);
 });
 
 client.on('guildCreate', async guild => {
