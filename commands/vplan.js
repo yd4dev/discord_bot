bedd6601e9aa6ce89d9710bc059a9e7846c57d14
Dennis1507 <yildirimdennis@aol.de>
2021-06-06 23:11:39 +0200

    Vplan function hopefully

--- commands/vplan.js
+++ commands/vplan.js
@@ -9,29 +9,28 @@ module.exports = {
 	description: 'Redacted',
 	args: false,
 	usage: ['0', '1'],
+	async getPlan(timetables, klasse, titles, day) {
+		const Embed = new Discord.MessageEmbed()
+			.setTitle(titles[day].innerHTML + ' | ' + klasse)
+			.setURL(timetables.data[0].url)
+			.setFooter('Zuletzt aktualisiert: ' + timetables.data[0].date);
+
+		const tableData = await scraper.get(timetables.data[0].url);
+
+		tableData[day].forEach(entry => {
+			if (entry['Klasse(n)'] == klasse) {
+				Embed.addField('__' + entry['(Fach)'] + (entry['(Fach)'] !== entry['Fach'] && entry['Fach'] != '' ? (' => ' + entry['Fach']) : '') + '__',
+					`St. ${entry['St.']} | Raum: ${entry['(Raum)']} ${(entry['(Raum)'] === entry['Raum'] ? '' : (' => ' + entry['Raum']))} \n ${entry['Art']} \n` + (entry['Hinweis'] !== '' ? 'Hinweis: ' + entry['Hinweis'] : ''));
+			}
+		});
+		if (Embed.fields.length < 1) {
+			Embed.setDescription('Keine Einträge für ' + klasse + ' gefunden.');
+		}
+		return Embed;
+	},
 	execute(message, args, client, prefix) {
 
 		if (message.guild.id != 623904281837305869 && message.guild.id != 658323643629174784) return;
-
-		async function getPlan(timetables, klasse, titles, day) {
-			const Embed = new Discord.MessageEmbed()
-				.setTitle(titles[day].innerHTML + ' | ' + klasse)
-				.setURL(timetables.data[0].url)
-				.setFooter('Zuletzt aktualisiert: ' + timetables.data[0].date);
-
-			const tableData = await scraper.get(timetables.data[0].url);
-
-			tableData[day].forEach(entry => {
-				if (entry['Klasse(n)'] == klasse) {
-					Embed.addField('__' + entry['(Fach)'] + (entry['(Fach)'] !== entry['Fach'] && entry['Fach'] != '' ? (' => ' + entry['Fach']) : '') + '__',
-						`St. ${entry['St.']} | Raum: ${entry['(Raum)']} ${(entry['(Raum)'] === entry['Raum'] ? '' : (' => ' + entry['Raum']))} \n ${entry['Art']} \n` + (entry['Hinweis'] !== '' ? 'Hinweis: ' + entry['Hinweis'] : ''));
-				}
-			});
-			if (Embed.fields.length < 1) {
-				Embed.setDescription('Keine Einträge für ' + klasse + ' gefunden.');
-			}
-			return Embed;
-		}
 
 		const dsb = new DSB('152902', 'Goethe');
 
@@ -46,16 +45,16 @@ module.exports = {
 				// titles[0].innerHTML
 
 				if (args[0] === '0') {
-					message.channel.send(await getPlan(timetables, 'E1/2', titles, 0));
+					message.channel.send(await this.getPlan(timetables, 'E1/2', titles, 0));
 				}
 				else if (args[0] === '1') {
-					message.channel.send(await getPlan(timetables, 'E1/2', titles, 1));
+					message.channel.send(await this.getPlan(timetables, 'E1/2', titles, 1));
 				}
 				else if (new Date().getUTCHours() < 13 && titles[0].innerHTML.startsWith(new Date().toLocaleDateString())) {
-					message.channel.send(await getPlan(timetables, 'E1/2', titles, 0));
+					message.channel.send(await this.getPlan(timetables, 'E1/2', titles, 0));
 				}
 				else {
-					message.channel.send(await getPlan(timetables, 'E1/2', titles, 1));
+					message.channel.send(await this.getPlan(timetables, 'E1/2', titles, 1));
 				}
 			})
 			.catch(e => {
---
+++ features/servers/vplan_discordchan.js
@@ -0,0 +1,39 @@
+// Discord Chan
+const DSB = require('dsbapi');
+const axios = require('axios');
+const { JSDOM } = require('jsdom');
+
+module.exports = client => {
+
+	if (client.user.id != '668275621461360642') return;
+
+	const channel = client.guilds.cache.get('623904281837305869').channels.cache.get('851159334930481224');
+
+	(function checkForUpdate() {
+		console.log('Running Function checkForUpdate() in vplan_discordchan');
+		const lastUpdate = new Date(channel.topic);
+		if ((lastUpdate.getDay() < new Date().getDay() || lastUpdate.getMonth() < new Date().getMonth() || lastUpdate.getYear() < new Date().getYear()) && new Date().getUTCHours() > 4) {
+
+			console.log('Checking for new Vertretungsplan Update');
+
+			const dsb = new DSB('152902', 'Goethe');
+
+			dsb.fetch()
+				.then(async data => {
+					const timetables = DSB.findMethodInData('timetable', data);
+
+					const html = await axios.get(timetables.data[0].url);
+					const dom = new JSDOM(html.data);
+
+					const titles = dom.window.document.querySelectorAll('.mon_title');
+
+					if (titles[0].innerHTML.startsWith(new Date().toLocaleDateString())) {
+						channel.send(await require('../../commands/vplan').getPlan(timetables, 'E1/2', titles, 0));
+						channel.setTopic(Date.now());
+					}
+				});
+		}
+		setInterval(checkForUpdate(), 90000);
+	})();
+
+};
