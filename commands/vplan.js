5d8f04a1ad32d879e78e918f8fee939cc0421603
Dennis1507 <yildirimdennis@aol.de>
2021-06-11 17:28:14 +0200

    ✨ VPlan Link Courses

--- commands/vplan.js
+++ commands/vplan.js
@@ -9,7 +9,7 @@ module.exports = {
 	description: 'Redacted',
 	args: false,
 	usage: ['0', '1'],
-	async getPlan(timetables, klasse, titles, day) {
+	async getPlan(timetables, klasse, titles, day, vplanUsers) {
 		const Embed = new Discord.MessageEmbed()
 			.setTitle(titles[day].innerHTML + ' | ' + klasse)
 			.setURL(timetables.data[0].url)
@@ -19,6 +19,13 @@ module.exports = {
 
 		tableData[day].forEach(entry => {
 			if (entry['Klasse(n)'] == klasse) {
+				const users = [];
+
+				vplanUsers.forEach(user => {
+					console.log(user);
+					if (user.some(fach => fach == klasse.replace(/\s+/g, '').toLowerCase())) console.log(user);
+				});
+
 				Embed.addField('__' + entry['(Fach)'] + (entry['(Fach)'] !== entry['Fach'] && entry['Fach'] != '' ? (' => ' + entry['Fach']) : '') + '__',
 					`St. ${entry['St.']} | Raum: ${entry['(Raum)']} ${(entry['(Raum)'] === entry['Raum'] ? '' : (' => ' + entry['Raum']))} \n ${entry['Art']} \n` + (entry['Hinweis'] !== '' ? 'Hinweis: ' + entry['Hinweis'] : ''));
 			}
@@ -32,6 +39,25 @@ module.exports = {
 
 		if (message.guild.id != 623904281837305869 && message.guild.id != 658323643629174784) return;
 
+		if (args[0] === 'link') {
+			args.shift();
+			if (!args[0]) return message.channel.send('Please provide arguments.');
+			(async () => {
+				args = args.join(' ');
+				args = args.split(',');
+
+				for (let i = 0; i < args.length; i++) {
+					args[i] = args[i].replace(/\s+/g, '').toLowerCase();
+				}
+				const UsersMap = client.data.guilds.get(message.guild.id).vplanUsers || new Map();
+
+				UsersMap.set(message.author.id, args);
+
+				await client.data.save(message.guild.id, client, { vplanUsers: UsersMap });
+			})();
+			return;
+		}
+
 		const dsb = new DSB('152902', 'Goethe');
 
 		dsb.fetch()
@@ -44,18 +70,20 @@ module.exports = {
 				const titles = dom.window.document.querySelectorAll('.mon_title');
 				// titles[0].innerHTML
 
+				const vplanUsers = client.data.guilds.get(message.guild.id).vplanUsers;
+
 				if (titles[0]) {
 					if (args[0] === '0') {
-						message.channel.send(await this.getPlan(timetables, 'E1/2', titles, 0));
+						message.channel.send(await this.getPlan(timetables, 'E1/2', titles, 0, vplanUsers));
 					}
 					else if (args[0] === '1') {
-						message.channel.send(await this.getPlan(timetables, 'E1/2', titles, 1));
+						message.channel.send(await this.getPlan(timetables, 'E1/2', titles, 1, vplanUsers));
 					}
 					else if (new Date().getUTCHours() < 13 && titles[0].innerHTML.startsWith(new Date().toLocaleDateString('de-DE'))) {
-						message.channel.send(await this.getPlan(timetables, 'E1/2', titles, 0));
+						message.channel.send(await this.getPlan(timetables, 'E1/2', titles, 0, vplanUsers));
 					}
 					else {
-						message.channel.send(await this.getPlan(timetables, 'E1/2', titles, 1));
+						message.channel.send(await this.getPlan(timetables, 'E1/2', titles, 1, vplanUsers));
 					}
 				}
 				else {
--- index.js
+++ index.js
@@ -12,8 +12,8 @@ client.commands = new Discord.Collection();
 client.schemas = new Discord.Collection();
 
 client.once('ready', async () => {
-	await require('./mongo.js')();
 	require('./load.js')(client);
+	await require('./mongo.js')();
 
 	client.guilds.cache.forEach(async guild => {
 		console.log(guild.name);
@@ -28,7 +28,7 @@ client.on('message', async message => {
 });
 
 client.on('guildCreate', async guild => {
-	client.cache.guilds.set(guild.id, await client.schemas.get('guild').findOne({ _id: guild.id }) || new Object());
+	client.cache.guilds.get(guild.id, await client.schemas.get('guild').findOne({ _id: guild.id }) || new Object());
 });
 
 client.login(process.env.token);
