005377950d8184283977815e7d49668f89dff751
Dennis1507 <yildirimdennis@aol.de>
2021-06-11 17:40:48 +0200

    ✨ Server Stats Channels

---
+++ commands/settings/stats.js
@@ -0,0 +1,13 @@
+module.exports = {
+	name: 'stats',
+	description: 'Create Server stats channels.',
+	args: false,
+	guild: true,
+	permissions: 'ADMINISTRATOR',
+	usage: [''],
+	async execute(message, args, client, prefix) {
+
+		/**/
+
+	},
+};
