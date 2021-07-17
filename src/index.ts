70837d493b88b5ea892be034ec4d7323690e6f50
dennis1507 <yildirimdennis@aol.de>
2021-07-17 20:05:34 +0200

    ✅ TS Tests

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import * as Discord from 'discord.js';
+
+const client = new Discord.Client();
+
+require('./run.ts')();
+
+console.log('hello')
+
+client.on('message', (message: Discord.Message) => {
+
+})
+
+//client.login('')
