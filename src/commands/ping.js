9a6434237ad46e3a45063044ea3542bcc1e60631
dennis1507 <yildirimdennis@aol.de>
2021-08-22 00:04:37 +0200

    Basic command handling i failed

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -1,10 +1,15 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
 module.exports = {
-	name: 'ping',
-	description: 'Pong!',
-	async execute(interaction, client) {
-		const reply = await interaction.reply('Pinging...');
-		console.log(reply);
-		const ping = reply.createdTimestamp - interaction.createdTimestamp;
-		await interaction.editReply(`🏓 Latency: ${ping}ms. API Latency is ${Math.round(client.ws.ping)}ms`);
+	data: new SlashCommandBuilder()
+		.setName('ping')
+		.setDescription('Pong!'),
+	async execute(interaction) {
+		await interaction.deferReply();
+		interaction.fetchReply()
+			.then(async reply => {
+				const ping = reply.createdTimestamp - interaction.createdTimestamp;
+				await interaction.editReply(`🏓 Latency: ${ping}ms. API Latency is ${Math.round(interaction.client.ws.ping)}ms`);
+			});
 	},
 };
