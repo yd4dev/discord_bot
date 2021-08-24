0cf5e86e645e2ef2ad6f92ec5ba3edf0ab0d532f
dennis1507 <yildirimdennis@aol.de>
2021-08-24 21:59:46 +0200

    🐛 Fixed ping command

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -7,13 +7,13 @@ module.exports = {
 		.setName('ping')
 		.setDescription('Pong!'),
 	async execute(interaction: CommandInteraction) {
-		await interaction.deferReply()
-			.then(async reply => { console.log(reply);});
-		interaction.fetchReply()
+		await interaction.deferReply({ fetchReply: true })
 			.then(async reply => {
-				console.log(typeof reply);
-				// const ping = reply.createdTimestamp - interaction.createdTimestamp;
-				// await interaction.editReply(`🏓 Latency: ${ping}ms. API Latency is ${Math.round(interaction.client.ws.ping)}ms`);
+
+				if (reply instanceof Message) {
+					const ping = reply.createdTimestamp - interaction.createdTimestamp;
+					await interaction.editReply(`🏓 Latency: ${ping}ms. API Latency is ${Math.round(interaction.client.ws.ping)}ms`);
+				}
 			});
 	},
 };
