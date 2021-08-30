3b63f8a329e6797c167f40a4dc4ab457bc033709
dennis1507 <yildirimdennis@aol.de>
2021-08-30 22:42:38 +0200

    commit

--- src/commands/moderation/mute.ts
+++ src/commands/moderation/mute.ts
@@ -1,7 +1,7 @@
 import { CommandInteraction } from 'discord.js';
 import { SlashCommandSubcommandBuilder } from '@discordjs/builders';
+import { SlashCommandBuilder } from '@discordjs/builders';
 
-const { SlashCommandBuilder } = require('@discordjs/builders');
 const { guild: guildSchema } = require('../../schemas/guild');
 const { mute: muteSchema } = require('../../schemas/mute');
 
@@ -16,12 +16,11 @@ module.exports = {
 			.addStringOption(option => option.setName('reason').setDescription('Why do you want to mute the user?')))
 		.addSubcommand((subcommand: SlashCommandSubcommandBuilder) => subcommand
 			.setName('setup')
-			.setDescription('Set up a muted role.')),
+			.setDescription('Set up a muted role for this server.')),
 	async execute(interaction: CommandInteraction) {
 		switch (interaction.options.getSubcommand()) {
 		case 'user':
-			console.log('usr');
-			break;
+			console.log(interaction.options.getUser('user', true));
 		}
 	},
 };
