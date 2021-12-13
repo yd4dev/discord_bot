03cdbf96f6c3eeac46128796b282a36129fdba76
dennis1507 <yildirimdennis@aol.de>
2021-12-13 19:04:44 +0100

    Remove waifus module

--- src/schemas/guild.ts
+++ src/schemas/guild.ts
@@ -2,8 +2,6 @@ import mongoose from 'mongoose';
 
 const guildSchema = new mongoose.Schema({
 	_id: { type: String, required: true },
-	waifusChannel: { type: String },
-	waifusMessage: { type: String },
 }, { versionKey: false });
 
 export default mongoose.model('guild', guildSchema);
